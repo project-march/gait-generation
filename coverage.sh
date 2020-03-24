1c9fb2620ecf8122d27df10db276b4b8b2e84557
Olav de Haas <o.n.dehaas@student.tudelft.nl>
2020-03-24 09:57:46 +0100

    Fix package name

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 cd build/march_rqt_gait_generator || return
-coverage xml --rcfile ../../src/gait-generator/.coveragerc
+coverage xml --rcfile ../../src/gait-generation/.coveragerc
 cd ../.. || return
